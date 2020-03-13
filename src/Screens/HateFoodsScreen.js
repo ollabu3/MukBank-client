090ad9e685919442e2ad2e79685fe689c0f9d345
YunJu Lee <ollabu3@gmail.com>
2020-03-13 21:49:49 +0900

    React-navigation 설치 및 states 구성

--- src/Screens/HateFoodsScreen.js
+++ src/Screens/HateFoodsScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-export default function HateScreen() {
+export default function HateFoodsScreen() {
   return (
     <View>
       <Text>Hate</Text>

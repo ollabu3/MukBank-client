0d1a1baf4ca0804943ab408589824d11c9b45928
michael00987 <hyeongkwangseok@gmail.com>
2020-03-16 13:27:56 +0900

    Fix : codeStyle

--- App.js
+++ App.js
@@ -1,16 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import HateFoodsScreen from './src/Screens/HateFoodsScreen';
+import HateFoodsScreen from './src/Screens/HateFood/HateFoodsScreen';
 import IntroScreen from './src/Screens/IntroScreen';
 import LoginScreen from './src/Screens/LoginScreen';
-import MapScreen from './src/Screens/MapScreen';
+import MapScreen from './src/Screens/Map/MapScreen';
 import MyPageScreen from './src/Screens/MyPageScreen';
-import PlaceListScreen from './src/Screens/PlaceListScreen';
 import RecommendBtnScreen from './src/Screens/RecommendBtnScreen';
+import MainPlaceScreen from './src/Screens/PlaceList/MainPlaceScreen';
 
 const Stack = createStackNavigator();
 
@@ -27,8 +26,13 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="Intro" component={IntroScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="HateFoods" component={HateFoodsScreen} />
+        <Stack.Screen name="Recommend" component={RecommendBtnScreen} />
+        <Stack.Screen name="MainPlace" component={MainPlaceScreen} />
+        <Stack.Screen name="Map" component={MapScreen} />
+        <Stack.Screen name="MyPage" component={MyPageScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );

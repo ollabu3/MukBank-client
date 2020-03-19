86ae2e6b2173f6debb9c0a4a6c67a640324fb733
michael00987 <hyeongkwangseok@gmail.com>
2020-03-20 01:57:30 +0900

    Fix : MapSceen.js expo모듈을 react-native 모둘로 교체

--- src/Screens/Map/MapScreen.js
+++ src/Screens/Map/MapScreen.js
@@ -1,83 +1,69 @@
-import React, { useState } from 'react';
-import { Platform, Text, View, Dimensions } from 'react-native';
-import MapView, { Marker, Polyline } from 'react-native-maps';
-import Constants from 'expo-constants';
-import * as Location from 'expo-location';
-import * as Permissions from 'expo-permissions';
+import React, { useState, useEffect } from 'react';
+import { Platform, Text, View, Dimensions, Image } from 'react-native';
+import MapView, { Marker, Polyline, Callout, Circle } from 'react-native-maps';
 import { ScrollView } from 'react-native-gesture-handler';
 import { locations } from './fakeData';
 
 export default function MapScreen() {
-  const [location, setLocation] = useState(null);
-  const [deslocations, setDesLocations] = useState(locations[0]);
-  const [errorMessage, setErrorMessage] = useState(null);
+  const [error, setError] = useState();
+  const [location, setLocation] = useState({
+    coords: { latitude: 32, longitude: 127 }
+  });
 
-  if (Platform.os === 'android' && !Constants.isDevice) {
-    setErrorMessage(
-      'Oops, this will not work on Sketch in an Android emulator. Try it on your device!'
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        setLocation(position);
+        setError(null);
+      }
+      // err => {
+      //   setError(err),
+      //     { enableHighAccuracy: true, timeout: 20000, maximumAge: 2000 };
+      // }
     );
-  } else {
-    _getLocationAsync();
-  }
-  async function _getLocationAsync() {
-    let { status } = await Permissions.askAsync(Permissions.LOCATION);
-    if (status !== 'granted') {
-      setErrorMessage('Permission to access location was denied');
-    }
-
-    let location = await Location.getCurrentPositionAsync({});
-    setLocation(location);
-  }
-
-  console.log('d', deslocations.coords);
-
-  let text = 'Waiting..';
-  if (errorMessage) {
-    text = errorMessage;
-  } else if (location) {
-    text = JSON.stringify(location);
-  }
+    console.log(location);
+  }, []);
   if (!location) {
     return (
       <View>
-        <Text>로딩중</Text>
+        <Text>loding</Text>
       </View>
     );
   }
-  const points = [
-    { latitude: 37.8025259, longitude: -122.4351431 },
-    { latitude: 37.7896386, longitude: -122.421646 },
-    { latitude: 37.7665248, longitude: -122.4161628 },
-    { latitude: 37.7734153, longitude: -122.4577787 },
-    { latitude: 37.7948605, longitude: -122.4596065 },
-    { latitude: 37.8025259, longitude: -122.4351431 }
-  ];
-
   return (
     <View>
-      <Text>{text}zzzaazz</Text>
-      <ScrollView>
-        <MapView
-          showsUserLocation
-          style={{
-            width: Dimensions.get('window').width,
-            height: 600
-          }}
-          region={{
-            latitude: location.coords.latitude,
-            longitude: location.coords.longitude
-            // latitudeDelta: 110,
-            // longitudeDelta: 110
-          }}
-        >
-          <MapView.Polyline
-            coordinates={points}
-            strokeWidth={10}
-            strokeColor="#00a8ff"
-            lineCap="around"
-          />
-        </MapView>
-      </ScrollView>
+      <Text>zsghfd</Text>
+      <MapView
+        showsUserLocation
+        style={{
+          width: Dimensions.get('window').width,
+          height: Dimensions.get('window').height
+        }}
+        region={{
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.0015,
+          longitudeDelta: 0.0021
+        }}
+      >
+        {/* <Polyline
+          coordinates={this.state.coordinate}
+          fillColor={'rgba(100,100,200,0,3)'}
+        /> */}
+        {/* <Circle
+          center={{ latitude: 37.612285, longitude: 127.031282 }}
+          radius={10000}
+          fillColor={'rgb(200,300,200,0.5)'}
+        /> */}
+        <Marker title="현위치" coordinate={location.coords}>
+          <Callout>
+            <Image
+              source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
+            />
+            <Text>Anasdfasdfaf city</Text>
+          </Callout>
+        </Marker>
+      </MapView>
     </View>
   );
 }

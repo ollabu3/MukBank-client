a80aa325e7906ce487dda4c797bf3be9e9860dc7
YunJu Lee <ollabu3@gmail.com>
2020-03-20 10:43:20 +0900

    Feat: Login시 hateScreen으로 navigate,Login시 server로 정보 전달 Fix: LoginScreen 틀 맞춤 - LoginScreen에서 googlesignin이 정상적으로 처리되면 hateScreen으로navigate하게 생성하였습니다. - 정상적으로 로그인 시 axios를 이용하여 server에게 user에대한 정보를 전달하도록 하였습니다(수정필요) - LoginScreen ui 틀을 만들었습니다

---
+++ src/Screens/Login/LoginScreen.js
@@ -0,0 +1,105 @@
+import React, { useState } from 'react';
+import {
+  ToastAndroid,
+  BackHandler,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity
+} from 'react-native';
+
+export default function LoginScreen({
+  navigation,
+  userinfo,
+  googleSignIn,
+  isLogin
+}) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.title}>
+        <Text style={styles.titleSize}>MukBank</Text>
+      </View>
+      <View style={styles.button}>
+        <View>
+          <TouchableOpacity style={styles.btn1}>
+            <Text style={styles.font1}>Facebook 로그인</Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <TouchableOpacity style={styles.btn2}>
+            <Text style={styles.font2}>Kakao 로그인</Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <TouchableOpacity
+            style={styles.btn3}
+            onPress={() =>
+              googleSignIn().then(() => {
+                if (isLogin === true) {
+                  navigation.replace('HateFoods');
+                }
+              })
+            }
+          >
+            <Text style={styles.font3}>Google 로그인</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: 'white'
+  },
+  title: {
+    flex: 3,
+    width: '100%',
+    height: '18%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  titleSize: {
+    fontSize: 40,
+    fontWeight: 'bold'
+  },
+  button: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  btn1: {
+    marginBottom: 10,
+    borderRadius: 5,
+    height: '50%',
+    backgroundColor: '#3b5998',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  font1: {
+    fontSize: 20
+  },
+  btn2: {
+    marginBottom: 10,
+    borderRadius: 5,
+    height: '50%',
+    backgroundColor: '#f7e600',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  font2: {
+    fontSize: 20
+  },
+  btn3: {
+    marginBottom: 10,
+    borderRadius: 5,
+    height: '50%',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  font3: {
+    fontSize: 20
+  }
+});

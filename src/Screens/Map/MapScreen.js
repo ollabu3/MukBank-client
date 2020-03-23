54ea3e5c5268e02fa02e84ff19086a262bac2bf5
michael00987 <hyeongkwangseok@gmail.com>
2020-03-23 11:17:14 +0900

    Feat : MapScreen.js Carousel 기능 추가
     - carousel을 옆으로 밀면 해당 데이터의 marker로 이동합니다.
     - marker를 클릭하면 carousel에 marker의 데이터가 출력됩니다.
     - fakeData를 숫자에서 실재 받아오는 값으로 수정했습니다.

--- src/Screens/Map/MapScreen.js
+++ src/Screens/Map/MapScreen.js
@@ -1,18 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import MapView, { Marker, Circle } from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView, { Marker, Circle, Callout } from 'react-native-maps';
 import Carousel from 'react-native-snap-carousel';
 import Geolocation from 'react-native-geolocation-service';
 import axios from 'axios';
 import { locations } from './fakeData';
 
 export default function MapScreen({ navigation }) {
+  let _carousel;
+  let _map;
+  let _marker; // 각각의 컴포넌트에서 가져온 것을 저장
   // 초기값 => 현재 위치
   const [location, setLocation] = useState({
     latitude: 37,
     longitude: 127
   });
-  const [desLocations, setDesLocations] = useState(locations); // 식당 데이터
+  const [desLocations, setDesLocations] = useState(locations); // 식당 데이터 배열
 
   // 식당 혹은 카페 정보 가져오기
   useEffect(() => {
@@ -27,10 +30,9 @@ export default function MapScreen({ navigation }) {
         parent: '음식점'
       }
     }).then(res => {
-      setDesLocations(locations);
       setDesLocations(res.data);
     });
-  });
+  }, [location]);
   // 현재위치 가져오기
   useEffect(() => {
     Geolocation.getCurrentPosition(position => {
@@ -44,9 +46,9 @@ export default function MapScreen({ navigation }) {
 
   // 스냅헸을때 가리키는 인덱스를 가지고 맵의 보이는 위치를 변환한다.
   function handleSnapToItem(index) {
-    console.log('snapped to ', index);
+    // console.log('snapped to ', index);
   }
-  let _carousel = {};
+
   /** */
 
   if (!location) {
@@ -57,17 +59,35 @@ export default function MapScreen({ navigation }) {
     );
   }
 
+  function onCarouselItemChange(index) {
+    _map.animateToRegion({
+      latitude: Number(desLocations[index].latitude),
+      longitude: Number(desLocations[index].longitude),
+      latitudeDelta: 0.0019,
+      longitudeDelta: 0.0019
+    });
+    // _marker[index].showCallout();
+  }
+  function onMarkerPressed(item, index) {
+    _map.animateToRegion({
+      latitude: Number(item.latitude),
+      longitude: Number(item.longitude),
+      latitudeDelta: 0.0019,
+      longitudeDelta: 0.0019
+    });
+    _carousel.snapToItem(index);
+  }
+
   function _renderItem({ item, index }) {
     // console.log('rendering,', index, item);
     return (
-      <View>
-        <View
+      <View style={{ alignItems: 'center' }}>
+        {/* <View
           onPress={() => {
             _carousel.snapToItem(index);
           }}
-        ></View>
-        <Text>{item.name}</Text>
-        <Text>{item.address}</Text>
+        /> */}
+        <Text style={{ fontSize: 20 }}>{item.name}</Text>
         <Text
           onPress={() => {
             navigation.navigate('Direction');
@@ -75,36 +95,56 @@ export default function MapScreen({ navigation }) {
         >
           길찾기
         </Text>
+        <Text
+          onPress={() => {
+            console.log('따르릉~~~');
+          }}
+        >
+          전화 걸기
+        </Text>
       </View>
     );
   }
 
   return (
     <View style={styles.container}>
-      <Text>zsghfd</Text>
+      <Text>a</Text>
       <MapView
         showsUserLocation
+        ref={map => {
+          _map = map;
+        }}
         style={styles.map}
         region={{
           latitude: location.latitude,
           longitude: location.longitude,
-          latitudeDelta: 0.01,
-          longitudeDelta: 0.01
+          latitudeDelta: 0.02,
+          longitudeDelta: 0.02
         }}
       >
         {desLocations.map((item, index) => {
-          if (item.latitude && item.longitude) {
-            return (
-              <Marker
-                key={index}
-                coordinate={{
-                  latitude: Number(item.latitude),
-                  longitude: Number(item.longitude)
-                }}
-                title={item.name}
-              />
-            );
-          }
+          return (
+            <Marker
+              ref={marker => {
+                _marker = marker;
+                // console.log(marker);
+              }}
+              key={index}
+              coordinate={{
+                latitude: Number(item.latitude),
+                longitude: Number(item.longitude)
+              }}
+              title={item.name}
+              onPress={() => {
+                onMarkerPressed(item, index);
+              }}
+            >
+              <Callout style={{ alignItems: 'center' }}>
+                <Text style={{ fontWeight: 'bold' }}>{item.name}</Text>
+                <Text>{item.address}</Text>
+              </Callout>
+            </Marker>
+          );
         })}
         <Circle
           radius={500}
@@ -115,20 +155,27 @@ export default function MapScreen({ navigation }) {
           fillColor={'rgba(100, 200, 200, 0.3)'}
         />
       </MapView>
-      <View style={{ backgroundColor: 'skyblue' }}>
+      <View style={{ backgroundColor: 'skyblue', marginTop: 500 }}>
         <Carousel
+          // contentContainerStyle={{ bottom: 11111 }} // ?? 이건 모르겠네
+          // containerCustomStyle={{ bottom: 0 }} // 안에 있는 내용물들의 속성
+          // contentContainerCustomStyle={{ bottom: 0 }} // 내용물이 밖으로 안나가고 안에서...
           ref={c => {
             _carousel = c;
           }}
           data={desLocations}
           renderItem={_renderItem}
           onSnapToItem={handleSnapToItem}
-          sliderWidth={360}
-          itemWidth={256}
-          layout={'default'}
+          sliderWidth={Dimensions.get('window').width}
+          itemWidth={Dimensions.get('window').width}
+          layout="default"
           firstItem={0}
+          removeClippedSubviews={false}
+          onSnapToItem={index => {
+            onCarouselItemChange(index);
+          }}
         />
-        {console.log(_carousel)}
+        {/* {console.log(_carousel)} */}
       </View>
     </View>
   );
@@ -137,6 +184,8 @@ export default function MapScreen({ navigation }) {
 let styles = StyleSheet.create({
   container: { ...StyleSheet.absoluteFillObject },
   map: {
-    ...StyleSheet.absoluteFillObject
+    ...StyleSheet.absoluteFillObject,
+    marginTop: 0,
+    marginBottom: 80
   }
 });

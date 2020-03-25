03327d6b91d086532ce93d03be511409f5fb7ece
YunJu Lee <ollabu3@gmail.com>
2020-03-25 11:34:21 +0900

    SelectFoodOrCafeScreen page 삽입

---
+++ src/Screens/SelectFoodOrCafe/SelectFoodOrCafeScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function SelectFoodOrCafeScreen() {
+  return (
+    <View>
+      <Text>SelectFoodOrCafe</Text>
+    </View>
+  );
+}

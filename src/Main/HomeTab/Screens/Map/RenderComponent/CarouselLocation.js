7cd1b28dacdadfe980f4a4a1cda06c3aa7b68cb6
michael00987 <hyeongkwangseok@gmail.com>
2020-04-04 15:49:14 +0900

    Fix : 좋아요 메서드를 컴포넌트로 이동
     - MapScreen에서 실행되던 좋아요 관련 메서드를 컴포넌트에서 실행하므로써,state가 각각에 적용이 되게 하였습니다.

--- src/Main/HomeTab/Screens/Map/RenderComponent/CarouselLocation.js
+++ src/Main/HomeTab/Screens/Map/RenderComponent/CarouselLocation.js
@@ -1,19 +1,76 @@
-import React from 'react';
-import { View, Text, Alert, Linking } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Alert, Linking, AsyncStorage } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import { Row, Grid, Col } from 'react-native-easy-grid';
+import { Row, Grid } from 'react-native-easy-grid';
+import axios from 'axios';
 
 export default function CarouselLocation({
   styles,
   item,
   navigation,
   datas,
-  count,
   phone,
-  like,
-  postLike,
   selectedIndex
 }) {
+  const [like, setLike] = useState(false);
+  const [count, setCount] = useState(0);
+
+  async function getIsLike() {
+    const tokenStr = await AsyncStorage.getItem('jwt');
+    const token = await JSON.parse(tokenStr).jwt;
+    if (datas !== null) {
+      axios({
+        method: 'post',
+        url: 'https://mukbank.xyz:5001/user/userrestsel',
+        headers: { Authorization: `Bearer ${token}` },
+        data: {
+          rest_id: datas[selectedIndex].id
+        }
+      }).then(res => {
+        console.log('-----------------', res);
+        // setLike(res.data);
+      });
+    }
+    // console.log('토큰 값ㅇ~~~~', datas[selectedIndex].id);
+  }
+
+  function getLikeCount() {
+    if (datas !== null) {
+      axios({
+        method: 'post',
+        url: 'https://mukbank.xyz:5001/restaurant/restlike',
+        data: {
+          rest_id: datas[selectedIndex].id
+        }
+      }).then(res => {
+        setCount(res.data);
+      });
+    }
+  }
+
+  async function postLike() {
+    const tokenStr = await AsyncStorage.getItem('jwt');
+    const token = await JSON.parse(tokenStr).jwt;
+    axios({
+      method: 'post',
+      url: 'https://mukbank.xyz:5001/restaurant/restlike',
+      headers: { Authorization: `Bearer ${token}` },
+      data: {
+        rest_id: datas[selectedIndex].id
+      }
+    }).then(() => {
+      setLike(like ? false : true);
+    });
+  }
+
+  useEffect(() => {
+    getLikeCount();
+  }, [like]);
+
+  useEffect(() => {
+    getIsLike();
+  }, []);
+
   return (
     <>
       <View style={styles.detailContainer}>

a78b687b513e1857ba71ef682cbd368e610a7b5e
YunJu Lee <ollabu3@gmail.com>
2020-04-05 14:49:10 +0900

    Fix: LikeList에서 List LongPress시 리스트 삭제 기능 추가 - List를 LongPress 시 press된 rest_id를 retlikeudate api에 post하여  리스트 삭제 된 후 재 랜더링되게 설정하였습니다.

--- src/Main/MyTab/Screens/LikeList/Components/LikeListEntry.js
+++ src/Main/MyTab/Screens/LikeList/Components/LikeListEntry.js
@@ -1,13 +1,7 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity
-} from 'react-native';
-import { Grid, Col } from 'react-native-easy-grid';
-import { Avatar, Divider } from 'react-native-elements';
+import { View, AsyncStorage, TouchableOpacity, Alert } from 'react-native';
+import { Grid } from 'react-native-easy-grid';
+import axios from 'axios';
 
 import styles from './LikeListStyles';
 import LikeListImg from './ListComponents/LikeListImg';
@@ -17,11 +11,47 @@ export default function LikeListEntry({ list, navigation, parent }) {
   // console.log(route);
   // console.log(parent);
   // console.log(list);
+
+  async function removeList() {
+    //토큰받고
+    const tokenStr = await AsyncStorage.getItem('jwt');
+    const token = await JSON.parse(tokenStr).jwt;
+    // 리스트 보내고
+    axios({
+      method: 'post',
+      url: 'https://mukbank.xyz:5001/user/restlikeupdate',
+      headers: { Authorization: `Bearer ${token}` },
+      data: {
+        rest_id: list.rest_id
+      }
+    });
+  }
+
+  // 지우는 알람
+  const removeAlert = () => {
+    Alert.alert('MukBank', '리스트를 삭제하시겠습니까?', [
+      {
+        text: '아니요',
+        style: 'cancel',
+        onPress: () => null
+      },
+      {
+        text: '예',
+        onPress: async () => {
+          removeList();
+        }
+      }
+    ]);
+  };
+
   return (
     <View>
       <TouchableOpacity
         style={styles.LikeListBoxContatiner}
         activeOpacity={1.0}
+        // 길게누를시에는 리스트 삭제
+        onLongPress={() => removeAlert()}
+        // 누를 시 디테일로 이동
         onPress={() => {
           navigation.navigate('Detail', {
             id: list.rest_id,
